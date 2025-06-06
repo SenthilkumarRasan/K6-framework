66c63a16ec90b5d37231d8b342ed4219386e27ce
Senthilkumar Rasan <34168946+SenthilkumarRasan@users.noreply.github.com>
2025-06-06 17:10:54 -0500

    Add files via upload

---
+++ handleSummary.js
@@ -0,0 +1,9 @@
+const { htmlReport } = require('../utils/bundle.js');
+
+module.exports = {
+  generateSummary: function(data) {
+    return {
+      'summary.html': htmlReport(data),
+    };
+  }
+};
